c73d058d2a908a38f23c86be5c4a75a52ff3f3f5
diego.quijano-at-202227537182 <diego.quijano@mango-soft.com>
2023-02-23 14:08:57 -0500

    new task

--- exer.js
+++ exer.js
@@ -25,3 +25,30 @@ const ar = array.map(item => '---' + item + '+++')
 
 console.log(array)
 console.log(ar)
+
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const acumulator = arr.reduce((accumulator,currentValue) => accumulator + currentValue)
+
+console.log(acumulator)
+
+const prom = acumulator/arr.length
+
+console.log(prom)
+
+
+const arra = [1, 2, 3, 4]
+
+let list = 0;
+    for (let i = 0; i <= arra.length; i++) {
+        list = list + arra[i];
+        if(list % 2 == 0) {
+            console.log(list);
+        }
+    }
+
+function par (arra) {
+    
+}
