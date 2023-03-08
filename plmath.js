a8bb2204c09b6e20fe22bacfc4f0b8255200fa47
diego.quijano-at-202227537182 <diego.quijano@mango-soft.com>
2023-03-08 13:43:47 -0500

    cal moda

--- plmath.js
+++ plmath.js
@@ -77,6 +77,19 @@ function ordenarLista(listaDesordenada) {
     return lista;
 }
 
+function calcularModa(lista) {
+    const listaCount = {};
+    for (let i = 0; i < lista.length; i++) {
+        const elemento = lista[i];
+        if (listaCount[elemento]) {
+            listaCount[elemento] += 1;
+        } else {
+            listaCount[elemento] = 1;
+        }
+    }
+    console.log(listaCount);
+}
+
 
 const arr = [2, 4, 6, 1, 3, 5,]
 
