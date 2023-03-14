d92c6e58895059cb9e69d5048dd76466a03addcc
diego0096 <55153234+diego0096@users.noreply.github.com>
2023-03-14 17:38:39 -0500

    salary growth

--- analisis.js
+++ analisis.js
@@ -24,3 +24,28 @@ function medianaPorPersona(nombrePersona) {
     console.log(medianaSalarios);
     return medianaSalarios;
 }
+
+function proyeccionPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+    let porcentajesCrecimiento = [];
+
+    for(let i = 1; i < trabajos.length; i++) {
+        const salarioActual = trabajos[i].salario;
+        const salarioAnterior = trabajos[i - 1].salario;
+        const crecimiento = salarioActual - salarioAnterior;
+        const porcentajeCrecimiento = crecimiento / salarioAnterior;
+        console.log(porcentajeCrecimiento);
+        porcentajesCrecimiento.push(porcentajeCrecimiento)
+    }
+
+    const medianaPorcentajesCrecimiento = plMath.calcularMediana(porcentajesCrecimiento);
+
+    console.log({porcentajesCrecimiento, medianaPorcentajesCrecimiento})
+
+    const ultimoSalario = trabajos[trabajos.length-1].salario;
+    const aumento = ultimoSalario * medianaPorcentajesCrecimiento;
+    const nuevoSalario = ultimoSalario + aumento;
+
+    return nuevoSalario;
+}
