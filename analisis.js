961eadedc954a50be7fde630bd6bc7b0696aed98
diego0096 <55153234+diego0096@users.noreply.github.com>
2023-03-15 08:57:35 -0500

    growth salary

--- analisis.js
+++ analisis.js
@@ -49,3 +49,22 @@ function proyeccionPorPersona(nombrePersona) {
 
     return nuevoSalario;
 }
+
+//Analisis empresarial
+
+const empresas = {};
+// salarios.forEach(persona => tabla)
+for (persona of salarios) {
+    for (trabajo of persona.trabajos) {
+        if (!empresas[trabajo.empresa]) {
+            empresas[trabajo.empresa] = {}
+        }
+        if (!empresas[trabajo.empresa][trabajo.year]) {
+            empresas[trabajo.empresa][trabajo.year] = [];
+        }
+
+        empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+    }
+}
+
+console.log(empresas)
